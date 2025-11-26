6ae86fa71b0bda24a4b2e4ccb40d1e55ebfbbc29
VagadiyaZalak2607 <vagadiyazalak2211@gmail.com>
2025-11-26 17:13:55 +0530

    Updated code

---
+++ day3/for-loop.py
@@ -0,0 +1,16 @@
+#name in loop
+Name = "Zalak"
+for i in Name:
+    print(i)
+
+#print numbers using for loop
+num = [2,4,6,8,9,10,12,14,16,18,20,22,24,26]
+for i in num:
+    print(i)
+
+
+#print table 
+n = int(input("Enter n : "))
+
+for i in range(1, 11):
+    print(n, "*", i, "=", n * i)
