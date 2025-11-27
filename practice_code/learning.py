07c8008bf5e63877fd64ff3ed098d7b99b182901
VagadiyaZalak2607 <vagadiyazalak2211@gmail.com>
2025-11-27 17:51:30 +0530

    Updated code

--- practice_code/learning.py
+++ practice_code/learning.py
@@ -17,3 +17,24 @@ B = int(input("Enter side B:"))
 C = int(input("Enter side C:"))
 Area = (A + B + C)/2
 print("Area of triangle is : ",Area)
+
+#number is Odd or Even
+N = int(input("Enter Number:"))
+if(N%2==0):
+    print("Number is Even")
+else:
+    print("Number is Odd")
+
+#number is positive or negative
+N = int(input("Enter Number:"))
+if(N>=0):
+    print("Number is positive")
+else:
+    print("Number is Negative")
+
+#year is leap year or not
+Year = int(input("Enter Year:"))
+if(Year%4==0):
+    print(Year,"is leap Year and it has 366 days")
+else:
+    print(Year,"is not leap Year and it has 365 days")
