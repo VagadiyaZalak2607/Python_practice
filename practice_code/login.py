d3a1f8126dafcd10d65fd890ba84be51aeb0adf6
VagadiyaZalak2607 <vagadiyazalak2211@gmail.com>
2025-12-09 12:26:30 +0530

    Updated code

---
+++ practice_code/login.py
@@ -0,0 +1,21 @@
+username = "admin"
+password = "1234"
+Balance = 100000
+
+for attempt in range(3):
+    u = input("Enter username: ")
+    p = input("Enter password: ")
+
+    if u == username:
+        if p == password:
+            print("Login successful!")
+            print("Your bank Balance is",Balance,"RS")
+            break
+        else:
+            print("Incorrect Password! Try again.")
+    else:
+        print("Incorrect Username! Try again.")
+
+else:
+    print("Account locked! Too many attempts.")
+
