1c9e35dfd3484fc1ada069bc7f584ab5ce3c9c4b
VagadiyaZalak2607 <vagadiyazalak2211@gmail.com>
2025-12-13 12:25:41 +0530

    Updated code

--- practice_code/patterns.py
+++ practice_code/patterns.py
@@ -1,9 +1,9 @@
-# num = 1
-# for i in range(5):
-#     for j in range(1,i+1):
-#         print(num,end='')
-#         num+=1
-#     print()
+num = 1
+for i in range(5):
+    for j in range(1,i+1):
+        print(num,end='')
+        num+=1
+    print()
 
 for i in range(5):
     for j in range(5):
@@ -11,9 +11,9 @@ for i in range(5):
         i+=1
     print()
 
-# for i in range(5):
-#     for j in range(5):
-#         print(i,end='')
-#         i+=1
-#     print()
+for i in range(5):
+    for j in range(5):
+        print(i,end='')
+        i+=1
+    print()
 
