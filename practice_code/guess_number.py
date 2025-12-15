f9ebb7c7e63483a6ee5c226481bde594d7cb78f1
VagadiyaZalak2607 <vagadiyazalak2211@gmail.com>
2025-12-15 17:16:04 +0530

    Updated code

--- practice_code/guess_number.py
+++ practice_code/guess_number.py
@@ -1,18 +1,26 @@
 import random
+
 number = random.randint(1,100)
+
 for attempt in range(3):
+
     G_num = int(input("Guess the number: "))
+
     if(G_num>number):
         print(G_num, "is to big")
+
     elif(G_num<number):
         print(G_num, "is to small")
+
     else:
         print(G_num,"is correct guess")
+        print("congratulations!!!!!, You win!!!!!")
         break
-else:
-    print(f"Sorry! The correct number was {number}.")
 
-print("-----GAME OVER-----")
+else:
+    print("Sorry! The correct number was" ,number)
+
+print("------- GAME OVER -------")
 
 
 
