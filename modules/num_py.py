a6b77bbfc8455bb5afc8f4ef6c17b44aeae3c7a9
VagadiyaZalak2607 <vagadiyazalak2211@gmail.com>
2025-12-16 14:41:59 +0530

    Updated code

---
+++ modules/num_py.py
@@ -0,0 +1,9 @@
+import numpy
+arr = numpy.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
+print("array =",arr)
+
+#use np keyword instead of numpy
+import numpy as np
+print("version =",np.__version__)
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10,])
+print("array =",arr)
