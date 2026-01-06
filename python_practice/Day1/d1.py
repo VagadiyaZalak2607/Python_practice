5ce92de19fc2a0a7c1131ace394805d2fe10176d
VagadiyaZalak2607 <vagadiyazalak2211@gmail.com>
2026-01-06 17:44:36 +0530

    Updated code

---
+++ python_practice/Day1/d1.py
@@ -0,0 +1,157 @@
+# Write a Python program to print "Hello World".
+print("Hello World")
+
+# Write a Python program to print your name and age in one line.
+Name = str(input("Enter your name: "))
+Age = int(input("Enter your age: "))
+print("Name =",Name, "Age =",Age)
+
+# Write a Python program to print the sum of two numbers, 5 and 10.
+A = 5
+B = 10
+print(A + B)
+
+
+# Write a Python program to display a multi-line message using a single print statement.
+print("""
+hello
+how are you 
+what is your name""")
+
+# Write a Python program that prints the output of 5 + 3 * 2.
+print(5+3*2)
+
+# Print numbers from 1 to 5 using separate print statements.
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+
+# Print numbers from 1 to 5 in a single line
+print(1, end=" ")
+print(2, end=" ")
+print(3, end=" ")
+print(4, end=" ")
+print(5, end=" ")
+print(1,2,3,4,5)
+
+# Write a program with at least one comment explaining the code.
+print("hello world") #this will print hello world
+   
+# Explain the difference between # and """ """ for comments.
+# It is used for single line comment
+""" it will return multi line comment"""
+# Define two variables a and b and assign them numbers, then print their sum.
+a = 22
+b = 26
+print(a + b)
+
+# Swap the values of two variables in Python.
+Z = 26
+D = 22
+temp = Z 
+Z = D
+D = temp
+# Z,D = D,Z
+print("after swapping:",Z)
+print("after swapping:",D)
+
+# Name 5 basic Python data types
+# INT
+# FLOAT
+# STRING
+# BOOL
+# LIST
+
+# Assign a string to a variable x.
+x = "Zalak"
+print(x)
+
+# Assign an integer to a variable y.
+y = 26
+print(y)
+
+# Check the data type of variable x = "Python".
+x = "python"
+print(type(x))
+
+# Assign a float number to a variable pi and print it.
+pi = 3.14
+print(pi)
+
+# Floor divide 17 by 3.
+print("floor division:",17//3)
+
+# What is the difference between / and // in Python?
+# /  returns floating point values
+# // returns integer value
+
+# Convert integer 5 to a float.
+A = float(5)
+print(A)
+
+# Convert float 3.14 to integer.
+B = int(3.14)
+print(B)
+
+# Convert integer 10 to string.
+C = str(10)
+print(C)
+
+# Convert string "100" to integer.
+D = int("100")
+print(D)
+
+# Convert string "3.5" to float.
+E = float("3.5")
+print(E)
+
+# Write a program to print your name, age, and favorite number in one line.
+name = "zalak"
+age = 20
+favourite_number = 22
+print(name,age,favourite_number)
+
+# Assign values to two variables, add them, then print the sum.
+A = 22
+B = 26
+print(A+B)
+
+# Assign a float value, convert it to int, and print both values.
+a = 26.22
+b = int(a)
+print(b)
+
+# Create a variable for a boolean value True and print it.
+a = True
+print(a)
+
+# Write a program to multiply two numbers and print the result with a message.
+a = 22
+b = 26
+print("multiplication of a and b =",a*b)
+
+# Write a program to swap two numbers without using a temporary variable.
+Z = 26
+D = 22
+Z,D = D,Z
+print("after swapping:",Z)
+print("after swapping:",D)
+
+# Write a Python program to find the average of three numbers.
+a = 22
+b = 26
+c = 20
+print("Average of e numbers :",(a+b+c)/3)
+
+# Write a program to calculate the square of a number.
+a = 10
+square = a*a
+print("aquare of a =",square)
+
+# Write a program to calculate the area of a rectangle using variables for length and width
+l = 22
+w = 26
+area = w*l
+print("area of rectangle = ",area)
